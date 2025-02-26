3e67de3fd995bb78c192febb514759e608be4a22
Peter <24620376+NemesisGuy@users.noreply.github.com>
2025-02-26 16:24:56 +0200

    cleanup

--- src/main/java/za/ac/cput/Main.java
+++ src/main/java/za/ac/cput/Main.java
@@ -8,10 +8,7 @@ import za.ac.cput.factory.StudentFactory;
 import za.ac.cput.util.Helper;
 import za.ac.cput.util.IDGenerator;
 
-import java.rmi.server.UID;
-import java.sql.PreparedStatement;
-import java.util.Random;
-import java.util.UUID;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -20,27 +17,18 @@ public class Main {
         Course course2 = CoursesFactory.createCourse(2, "ADP 3", 30);
         Course course3 = CoursesFactory.createCourse(IDGenerator.generateIntId(), "PROJ MAN 3", 40);
         Course course4 = CoursesFactory.createCourse(IDGenerator.generateIntId(), " ADT 3 ", 40);
-        Course course5 = CoursesFactory.createCourse(Helper.generateIntId(), Helper.generateUUIdCozImCold(), 40);
-
+        Course course5 = CoursesFactory.createCourse(Helper.generateIntId(), "Prof Dev 3" , 40);
 
         Student student = StudentFactory.createStudent(Helper.generateUUIdCozImCold(),"John", 22);
-//Course course6 = new Course(builder);
 
-//Srting ID UUID;
-//Int id;
+
         System.out.println(course.toString());
         System.out.println(course2.toString());
         System.out.println(course3.toString());
         System.out.println(course4.toString());
-        System.out.println(student.toString());
-        System.out.println(course5.toString());
+        System.out.println(student.toString().toLowerCase());
+        System.out.println(course5.toString().toUpperCase());
 
-//        System.out.println(new Random().nextInt(Integer.MAX_VALUE));
-//        System.out.println(UUID.randomUUID().toString());
-
-
-//        System.out.println(course.toString());
-        //   System.out.println(course.toString());
     }
 
 }
--- src/main/java/za/ac/cput/factory/CoursesFactory.java
+++ src/main/java/za/ac/cput/factory/CoursesFactory.java
@@ -2,18 +2,19 @@ package za.ac.cput.factory;
 
 import za.ac.cput.domian.Course;
 
-public class CoursesFactory {
+public class CoursesFactory implements IFactory {
 
     public static Course createCourse (int id, String name, int credits){
         return new Course.Builder()
-
                 .setId(id)
                 .setName(name)
                 .setCredits(credits)
                 .build();
-
     }
 
 
-
+    @Override
+    public <T> T Create() {
+        return null;
+    }
 }
